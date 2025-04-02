be5ddfe9c3bc06f1cfb092a9ddf065a16784d5d8
December <jiale.gfi@outlook.com>
2025-04-02 23:35:45 +0800

    done

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "http://121.4.100.51:8003",
         changeOrigin: true,
       }
     }
